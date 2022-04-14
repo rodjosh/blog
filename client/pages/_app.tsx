c80bfb1ae41725d05b98957ea47ecca0fbd4fe77
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-04-14 07:14:14 -0600

    WIP

---
+++ client/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { Normalize } from "styled-normalize";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Normalize />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ client/routes/index.ts
@@ -0,0 +1,6 @@
+const routes = {
+  blog: "/blog",
+  admin: "/admin",
+};
+
+export default routes;
