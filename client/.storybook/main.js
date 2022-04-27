f4889f1ca7f89e97e07fcc8ec24eebaec8de0f12
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-04-27 08:27:26 -0600

    Fix storybook config for absolute paths

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,3 +1,5 @@
+const { resolve } = require("path");
+
 module.exports = {
   stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -6,4 +8,12 @@ module.exports = {
     "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules ?? []),
+      resolve(__dirname, "../"),
+    ];
+
+    return config;
+  },
 };
