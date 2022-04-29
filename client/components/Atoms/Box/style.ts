f00055b3eca8ba02108477437f3b85a832e3cf04
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-04-30 02:17:58 +0300

    box component 2

--- client/components/Atoms/Box/style.ts
+++ client/components/Atoms/Box/style.ts
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
-
 interface BoxProps {
-    $margin?: string;
-    $padding?: string;
-    $display?: string;
-  }
+	$margin?: string;
+	$padding?: string;
+	$display?: string;
+}
 
 export const StyleBox = styled.div<BoxProps>`
-    margin: ${({ $margin }) => $margin ?? "0"};
-    padding: ${({ $padding }) => $padding ?? "0"};
-    display: ${({ $display }) => $display ?? "flex"};
+	margin: ${({ $margin }) => $margin ?? "0"};
+	padding: ${({ $padding }) => $padding ?? "0"};
+	display: ${({ $display }) => $display ?? "block"};
 `;
