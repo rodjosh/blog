d97ccf8e1e4798244e0377a47358c1477f93c3fc
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-05-08 12:57:06 -0600

    Add basic Text component setup

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,14 @@
-import nextJest from "next/jest";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
+  //directory of next.config.js and jest.config.js
   dir: "./",
 });
+
+const customJestConfig = {
+  //directories of modules and typescript baseUrl files
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
