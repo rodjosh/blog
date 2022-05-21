ab4da7c3cc1b5d02846db3e165db7745314ea5bd
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-05-20 19:13:07 -0600

    Fix Navbar

--- client/components/Organisms/Navbar/variants.tsx
+++ client/components/Organisms/Navbar/variants.tsx
@@ -1,5 +1,5 @@
 import Text, { TextProps } from "components/Atoms/Text";
 
-export const TextNavItem = (props: TextProps) => (
-  <Text fontWeight={700} size={20} {...props} />
-);
+export const TextNavItem = (props: TextProps) => {
+  return <Text fontWeight={700} size={20} {...props} />;
+};
